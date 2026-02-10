6e36f207869489e3d6249aa4c58411c65f818d10
matheus3070 <matheeus.estudos2002@gmail.com>
2026-02-09 23:16:54 -0300

    tabuada simples js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log("Olá Mundo, isso é um teste para github");
 
 console.log("vamo ver se sei fazer sozinho");
+
+let multiplicador = 20;
+let contador
+ 
+for (let i = 0; i <= 10; i++){
+    contador = multiplicador * i;
+    console.log(multiplicador + " x " + i + " = " + contador);
+}
+
+
+console.log("código de tabuada js simples para teste no github");
